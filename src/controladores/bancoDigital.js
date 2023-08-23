288cc734a7137c61f90efee9b262b13a30137c68
RenataUzeda <renatauzedasilva@gmail.com>
2023-08-23 11:46:53 -0300

    feat: implementa funcionalidade exibir extrato conta
    Cria código para exibir o extrato da movimentação de uma conta bancária.
    
    Autora: Renata Uzêda.

--- src/controladores/bancoDigital.js
+++ src/controladores/bancoDigital.js
@@ -286,7 +286,7 @@ const transferir = (req, res) => {
 
   transferencias.push(registroTransferencia);
 
-  console.log(registroTransferencia);
+  //console.log(registroTransferencia);
 
   return res.status(204).json();
 };
@@ -311,7 +311,33 @@ const saldo = (req, res) => {
   return res.status(200).json({ saldo: conta.saldo });
 };
 
-const extrato = (req, res) => {};
+const extrato = (req, res) => {
+  const { numero_conta, senha } = req.query;
+
+  if (!numero_conta || !senha) {
+    return res.status(400).json({ mensagem: "Número da conta e senha são obrigatórios!" });
+  }
+
+  const conta = contas.find(conta => conta.numero === numero_conta);
+
+  if (!conta) {
+    return res.status(404).json({ mensagem: "Conta bancária não encontrada!" });
+  }
+
+  if (conta.usuario.senha !== senha) {
+    return res.status(401).json({ mensagem: "Senha inválida!" });
+  }
+
+  // Filtrar as transações relacionadas a essa conta
+  const transacoesConta = {
+    depositos: depositos.filter(transacao => transacao.numero_conta === numero_conta),
+    saques: saques.filter(transacao => transacao.numero_conta === numero_conta),
+    transferenciasEnviadas: transferencias.filter(transacao => transacao.numero_conta_origem === numero_conta),
+    transferenciasRecebidas: transferencias.filter(transacao => transacao.numero_conta_destino === numero_conta),
+  };
+
+  return res.status(200).json(transacoesConta);
+};
 
 module.exports = {
   listarContasBancarias,
